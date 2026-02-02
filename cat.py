1ad4a7e30451895f8607a0e9f24a26fbb2686e6e
Michaela Yeung <yeung.michaela@gmail.com>
2026-02-02 15:41:50 -0800

    testing python

--- cat.py
+++ cat.py
@@ -1,11 +1,12 @@
-'''
-This program prints stdin to the screen.
-'''
 import sys
 
 def cat(file):
-    data = file.read()
-    sys.stdout.buffer.write(data)
+    chunk_size = 4096
+    while True:
+        chunk = file.read(chunk_size)
+        if not chunk:
+            break
+        sys.stdout.buffer.write(chunk)
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
@@ -14,3 +15,4 @@ if __name__ == "__main__":
                 cat(f)
     else:
         cat(sys.stdin.buffer)
+
